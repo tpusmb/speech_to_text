a4f0f62ac2b7667a0b9b9abea1d0c1c227b2176c
Hugo CHASTEL <hugo.chastel@orange.com>
2020-02-12 08:44:39 +0100

    Added better support of noise level for speech to text

--- basic_speech_to_text.py
+++ basic_speech_to_text.py
@@ -1,34 +1,31 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 """
-File to manage the speech to text on our robots.
+File to manage the speech to text for our plant.
 """
+from typing import Union
 
 import speech_recognition as sr
 
 
-def speech_to_text():
+def speech_to_text(noise_level: int = None) -> Union[None, str]:
     """
     Function called to listen and convert to text the answer of the user.
     Note that the user have to talk in french.
-    :return: (string) The answer of the user
+    :param: noise_level the level of ambient noise used to detect the end of a phrase
+    :return: The answer of the user
     """
     r = sr.Recognizer()
     with sr.Microphone() as source:
+        if noise_level:
+            r.energy_threshold = noise_level
+        else:
+            r.adjust_for_ambient_noise(source)
         audio = r.listen(source)
 
     try:
-        # Call the google voice recognizer and put the answer in the local
-        # variable "text" as String
-        text = r.recognize_google(audio, language="fr-FR")
-
-        # Return a string containing the answer of the user
-        return text
-    except sr.UnknownValueError:
-        return "None"
-    except sr.RequestError as e:
-        return "None"
+        # Call the google voice recognizer
+        return r.recognize_google(audio, language="fr-FR")
+    except (sr.UnknownValueError, sr.RequestError):
+        return
 
 
 if __name__ == "__main__":
